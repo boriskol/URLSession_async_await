edad02acda937e474f2cb9f3f815c472f775a83d
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-11 13:11:11 -0600

    Initial Commit

---
+++ Shared/URLSession_async_awaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_async_awaitApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_async_awaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
